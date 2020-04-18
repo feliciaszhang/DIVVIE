5fe61e5f84f1f4da7aa144bcec6aa7ef978cbefb
feliciaszhang <feliciaszhang@gmail.com>
2020-04-17 22:08:16 -0400

    result fragment

--- app/src/main/java/com/example/divvie/DivvieViewModel.kt
+++ app/src/main/java/com/example/divvie/DivvieViewModel.kt
@@ -30,9 +30,24 @@ class DivvieViewModel(application: Application) : AndroidViewModel(application) 
     val taxObservable: LiveData<Double>
         get() = tax
 
+    private val tip = MutableLiveData<Double>()
+    val tipObservable: LiveData<Double>
+        get() = tip
+
+    private val total = MutableLiveData<Double>()
+    val totalObservable: LiveData<Double>
+        get() = total
+
     private val pretaxListOfPrices = MutableLiveData<List<Double>>()
     val pretaxListOfPricesObservable: LiveData<List<Double>>
         get() = pretaxListOfPrices
+
+    private fun setTotal() {
+        val subtotal: Double = getSubtotal() ?: 0.0
+        val tax: Double = getTax() ?: 0.0
+        val tip: Double = getTip() ?: 0.0
+        total.value = subtotal + tax + tip
+    }
 
     fun setDisplayPrices(bool: Boolean) {
         displayPrices.value = bool
@@ -40,6 +55,7 @@ class DivvieViewModel(application: Application) : AndroidViewModel(application) 
 
     fun setSubtotal(num: Double) {
         subtotal.value = num
+        setTotal()
     }
 
     fun getSubtotal(): Double? {
@@ -48,10 +64,20 @@ class DivvieViewModel(application: Application) : AndroidViewModel(application) 
 
     fun setTax(num: Double) {
         tax.value = num
+        setTotal()
     }
 
     fun getTax(): Double? {
         return tax.value
+    }
+
+    fun setTip(num: Double) {
+        tip.value = num
+        setTotal()
+    }
+
+    fun getTip(): Double? {
+        return tip.value
     }
 
     fun setPretaxListOfPrices(list: List<Double>) {
--- app/src/main/java/com/example/divvie/fragments/ResultFragment.kt
+++ app/src/main/java/com/example/divvie/fragments/ResultFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import com.example.divvie.AMOUNT_DEFAULT
 import com.example.divvie.DivvieViewModel
 import com.example.divvie.R
 
@@ -14,14 +19,22 @@ class ResultFragment : Fragment() {
         fun newInstance() = ResultFragment()
     }
     private lateinit var viewModel: DivvieViewModel
-
+    private lateinit var subtotal: TextView
+    private lateinit var tax: TextView
+    private lateinit var tip: EditText
+    private lateinit var total: TextView
+    private lateinit var start_over: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val fragment = inflater.inflate(R.layout.result_fragment, container, false)
-
+        subtotal = fragment.findViewById(R.id.subtotal_amount)
+        tax = fragment.findViewById(R.id.tax_amount)
+        tip = fragment.findViewById(R.id.edit_tip)
+        total = fragment.findViewById(R.id.total_amount)
+        start_over = fragment.findViewById(R.id.start_over)
 
         return fragment
     }
@@ -29,5 +42,35 @@ class ResultFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(activity!!).get(DivvieViewModel::class.java)
+        viewModel.setTip(AMOUNT_DEFAULT)
+        viewModel.subtotalObservable.observe(viewLifecycleOwner, Observer { displaySubtotal(it) })
+        viewModel.taxObservable.observe(viewLifecycleOwner, Observer{  displayTax(it) })
+        viewModel.tipObservable.observe(viewLifecycleOwner, Observer{  displayTip(it) })
+        viewModel.totalObservable.observe(viewLifecycleOwner, Observer{  displayTotal(it) })
+
+        tip.setOnFocusChangeListener { v, hasFocus ->
+            if (!hasFocus) {
+                if (tip.text.toString() != "") {
+                    val num = tip.text.toString().toDouble()
+                    viewModel.setTip(num)
+                }
+            }
+        }
+    }
+
+    private fun displaySubtotal(num: Double) {
+        subtotal.text = num.toString()
+    }
+
+    private fun displayTax(num: Double) {
+        tax.text = num.toString()
+    }
+
+    private fun displayTip(num: Double) {
+        tip.hint = num.toString()
+    }
+
+    private fun displayTotal(num: Double) {
+        total.text = num.toString()
     }
 }
