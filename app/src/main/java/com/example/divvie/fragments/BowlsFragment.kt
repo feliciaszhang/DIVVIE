657f0dff1ae3e3fa023273c42171eded42366481
feliciaszhang <feliciaszhang@gmail.com>
2020-04-19 02:38:07 -0400

    change bowl color

--- app/src/main/java/com/example/divvie/fragments/BowlsFragment.kt
+++ app/src/main/java/com/example/divvie/fragments/BowlsFragment.kt
@@ -1,9 +1,13 @@
 package com.example.divvie.fragments
 
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -14,7 +18,7 @@ import com.example.divvie.R
 import com.example.divvie.DivvieViewModel
 import com.example.divvie.NUMBER_OF_PEOPLE_DEFAULT
 import com.example.divvie.database.Person
-import kotlinx.android.synthetic.main.bowl.view.*
+import kotlinx.android.synthetic.main.bowl.*
 
 class BowlsFragment : Fragment() {
     companion object {
@@ -31,6 +35,11 @@ class BowlsFragment : Fragment() {
     ): View {
         val fragment = inflater.inflate(R.layout.bowls_fragment, container, false)
         bowlsList = fragment.findViewById(R.id.bowls)
+        for (i in 0 until MAX_NUMBER_OF_PEOPLE) {
+            val view = bowlsList.getChildAt(i)
+            val image: ImageView = view.findViewById(R.id.imageView)
+            changeColor(image, Color.LTGRAY)
+        }
 
         return fragment
     }
@@ -41,6 +50,11 @@ class BowlsFragment : Fragment() {
         viewModel.getNumberOfPeople().observe(viewLifecycleOwner, Observer { displayBowls(it) })
         viewModel.getAllPerson().observe(viewLifecycleOwner, Observer { updateBowls(it) })
         viewModel.displayPricesObservable.observe(viewLifecycleOwner, Observer { displayPrices(it) })
+        viewModel.selectPersonObservable.observe(viewLifecycleOwner, Observer { greyoutBowls(it) })
+    }
+
+    fun changeColor(image: ImageView, color: Int) {
+        image.colorFilter = PorterDuffColorFilter(color, PorterDuff.Mode.SRC_ATOP)
     }
 
     private fun displayBowls(num: Int) {
@@ -73,6 +87,21 @@ class BowlsFragment : Fragment() {
             val priceAmount: TextView = view.findViewById(R.id.price_amount)
             val personalTotal = person.subtotal + person.tax + person.tip
             priceAmount.text = personalTotal.toString()
+        }
+    }
+
+    private fun greyoutBowls(bool: Boolean) {
+        if (bool) {
+            for (i in 0 until MAX_NUMBER_OF_PEOPLE) {
+                val view = bowlsList.getChildAt(i)
+                view.isClickable = true
+                val image: ImageView = view.findViewById(R.id.imageView)
+                val currency: TextView = view.findViewById(R.id.currency)
+                val priceAmount: TextView = view.findViewById(R.id.price_amount)
+                currency.setTextColor(Color.DKGRAY)
+                priceAmount.setTextColor(Color.DKGRAY)
+                changeColor(image, Color.DKGRAY)
+            }
         }
     }
 }
--- app/src/main/java/com/example/divvie/fragments/InputFragment.kt
+++ app/src/main/java/com/example/divvie/fragments/InputFragment.kt
@@ -75,6 +75,7 @@ class InputFragment : Fragment() {
         }
 
         editSubtotalText.addTextChangedListener(object: TextWatcher {
+            // TODO remind user it's pretax
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
