75ba909eedeec96db2b1a3d05d78a9304fa4415a
feliciaszhang <feliciaszhang@gmail.com>
2020-04-21 14:49:11 -0400

    all view state and view event added

--- app/src/main/java/com/example/divvie/DivvieViewEvent.kt
+++ app/src/main/java/com/example/divvie/DivvieViewEvent.kt
@@ -1,9 +1,35 @@
 package com.example.divvie
 
 sealed class InputViewEvent {
-    object InsertPerson : InputViewEvent()
-    object RemovePerson : InputViewEvent()
-    object EnterSubtotal : InputViewEvent()
-    object EnterTax : InputViewEvent()
-    object Next : InputViewEvent()
+    object InsertPerson: InputViewEvent()
+    object RemovePerson: InputViewEvent()
+    object EnterSubtotal: InputViewEvent()
+    object EnterTax: InputViewEvent()
+    object Next: InputViewEvent()
+}
+
+sealed class SplitViewEvent {
+    object SplitEqually: SplitViewEvent()
+    object SplitIndividually: SplitViewEvent()
+    object Back: SplitViewEvent()
+}
+
+sealed class ItemViewEvent {
+    object EnterItemPrice: ItemViewEvent()
+    object Next: ItemViewEvent()
+    object Done: ItemViewEvent()
+    object Back: ItemViewEvent()
+    object ClearAll: ItemViewEvent()
+}
+
+sealed class ResultViewEvent {
+    object EnterTip: ResultViewEvent()
+    object ToggleFormat: ResultViewEvent()
+    object Back: ResultViewEvent()
+    object StartOver: ResultViewEvent()
+}
+
+sealed class BowlsViewEvent {
+    object ClickBowl: BowlsViewEvent()
+    object DisplayBreakdown: BowlsViewEvent()
 }
--- app/src/main/java/com/example/divvie/DivvieViewState.kt
+++ app/src/main/java/com/example/divvie/DivvieViewState.kt
@@ -1,8 +1,31 @@
 package com.example.divvie
 
+import android.graphics.Color
+import com.example.divvie.database.Item
+
 data class InputViewState(
     val numberOfPeople: Int = NUMBER_OF_PEOPLE_DEFAULT,
     val subtotal: Double = AMOUNT_DEFAULT,
     val tax: Double = AMOUNT_DEFAULT
+)
+
+data class ItemViewState(
+    val tempItemPrice: Double = AMOUNT_DEFAULT,
+    val tempLeftOverPrice: Double,
+    val currentItem: Item,
+    val finalLeftOverPrice: Double
+)
+
+data class ResultViewState(
+    val total: Double,
+    val tip: Double = AMOUNT_DEFAULT,
+    val format: Int = 0
+)
+
+data class BowlsViewState(
+    val color: Int = Color.LTGRAY,
+    val isClickable: Boolean = false,
+    val displayPrices: Boolean = false,
+    val numberOfBowls: Int
 )
 
