00391820790f8ffb808f603b4b334cd6211afc16
feliciaszhang <feliciaszhang@gmail.com>
2020-05-28 21:16:46 -0400

    unfocus edittext when pressed outside

--- app/src/main/java/com/example/divvie/MainActivity.kt
+++ app/src/main/java/com/example/divvie/MainActivity.kt
@@ -1,19 +1,25 @@
 package com.example.divvie
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.ViewModelProviders
 import com.example.divvie.fragments.BowlsFragment
 import com.example.divvie.fragments.InputFragment
+import android.graphics.Rect
+import android.widget.EditText
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: DivvieViewModel
 
     // TODO format number to 2 decimal
-    // TODO launcher background image
-    // TODO flash final split bowls
-    // TODO icons?
+    // TODO fix icon and launcher image
+    // TODO flash final split bowls?
+    // TODO icons for navigation?
     // TODO more colors?
+    // TODO fix bug where price is not cleared when restart
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -30,5 +36,21 @@ class MainActivity : AppCompatActivity() {
                 R.id.bowls_fragment_layout, BowlsFragment.newInstance()
             ).commit()
         }
+    }
+
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.windowToken, 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }
