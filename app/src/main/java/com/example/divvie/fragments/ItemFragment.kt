e3051ca0a00dd9b5b584c7968f5f73547612e698
feliciaszhang <feliciaszhang@gmail.com>
2020-06-15 16:07:39 -0400

    refactor item fragment

--- app/src/main/java/com/example/divvie/fragments/ItemFragment.kt
+++ app/src/main/java/com/example/divvie/fragments/ItemFragment.kt
@@ -24,8 +24,7 @@ class ItemFragment : Fragment() {
     }
     private lateinit var viewModel: DivvieViewModel
     private lateinit var editItemText: EditText
-    private lateinit var leftoverText: TextView
-    private lateinit var tap: TextView
+    private lateinit var itemHelper: TextView
     private lateinit var nextButton: Button
     private lateinit var doneButton: Button
     private lateinit var backButton: Button
@@ -40,8 +39,7 @@ class ItemFragment : Fragment() {
     ): View {
         val fragment = inflater.inflate(R.layout.item_fragment, container, false)
         editItemText = fragment.findViewById(R.id.edit_item)
-        leftoverText = fragment.findViewById(R.id.item_helper)
-        tap = fragment.findViewById(R.id.tap)
+        itemHelper = fragment.findViewById(R.id.item_helper)
         nextButton = fragment.findViewById(R.id.next)
         doneButton = fragment.findViewById(R.id.done)
         backButton = fragment.findViewById(R.id.back)
@@ -105,7 +103,6 @@ class ItemFragment : Fragment() {
             ).commit()
         }
         val tempLeftover = (viewState.leftover!!).toBigDecimal() - (viewState.tempItemPrice).toBigDecimal()
-        leftoverText.text = String.format(resources.getString(R.string.leftover), filter.clean(tempLeftover.toString()))
         doneButton.isEnabled = viewState.tempItemListOfIndex.size != 0
         nextButton.isEnabled = viewState.tempItemPrice != 0.0 && !viewState.invalidItem && tempLeftover.toDouble() >= 0.0
         clearAllButton.isEnabled = viewState.itemList.size > 0
@@ -122,30 +119,28 @@ class ItemFragment : Fragment() {
             editItemText.removeTextChangedListener(textWatcher)
             editItemText.setText(filter.clean(viewState.tempItemPrice.toString()))
             editItemText.addTextChangedListener(textWatcher)
-            leftoverText.visibility = View.GONE
+            itemHelper.text = resources.getString(R.string.tap)
             nextButton.visibility = View.GONE
-            tap.visibility = View.VISIBLE
             doneButton.visibility = View.VISIBLE
         } else {
             editItemText.requestFocus()
             editItemText.background = editTextBackground
-            leftoverText.visibility = View.VISIBLE
+            itemHelper.text = String.format(resources.getString(R.string.leftover), filter.clean(tempLeftover.toString()))
             nextButton.visibility = View.VISIBLE
-            tap.visibility = View.GONE
             doneButton.visibility = View.GONE
             if (!viewState.isItemEditing) {
                 editItemText.setText("")
             }
         }
         if (viewState.invalidItem) {
-            leftoverText.text = resources.getString(R.string.warning)
-            leftoverText.setTextColor(resources.getColor(R.color.colorAccent, context!!.theme))
+            itemHelper.text = resources.getString(R.string.warning)
+            itemHelper.setTextColor(resources.getColor(R.color.colorAccent, context!!.theme))
             editItemText.background.colorFilter = PorterDuffColorFilter(
                 resources.getColor(R.color.colorAccent, context!!.theme), PorterDuff.Mode.SRC_ATOP)
         }
-        if (!viewState.invalidItem) {
-            leftoverText.text = String.format(resources.getString(R.string.leftover), filter.clean(tempLeftover.toString()))
-            leftoverText.setTextColor(resources.getColor(R.color.colorWhite, context!!.theme))
+        if (!viewState.invalidItem && !viewState.isSplittingBowls) {
+            itemHelper.text = String.format(resources.getString(R.string.leftover), filter.clean(tempLeftover.toString()))
+            itemHelper.setTextColor(resources.getColor(R.color.colorWhite, context!!.theme))
             if (!viewState.isSplittingBowls) {
                 editItemText.background.colorFilter = PorterDuffColorFilter(
                     resources.getColor(R.color.colorLight, context!!.theme),
